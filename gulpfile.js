ec6f94ebe799ce9900962da76e657ee54991dd91
Andry <andry.yosua@pinteraktif.com>
2017-03-16 18:12:25 +0700

    update new teaser

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
-const gulp = require( 'gulp');
-const babel = require( 'gulp-babel');
-const sass = require( 'gulp-sass');
-const pug = require( 'gulp-pug');
-const plumber = require( 'gulp-plumber');
-const autoprefixer = require( 'gulp-autoprefixer');
-const cleanCSS = require( 'gulp-clean-css');
-const rename = require( "gulp-rename");
-const filter = require( 'gulp-filter');
-const uglify = require( 'gulp-uglify');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const filter = require('gulp-filter');
+const uglify = require('gulp-uglify');
 
-
-const babelOpts = {presets: ['es2015'], compact: false};
+const babelOpts = {
+    presets: ['es2015'],
+    compact: false
+};
 
 gulp.task('webHtml', () => {
     const pugOpts = {
@@ -18,33 +20,28 @@ gulp.task('webHtml', () => {
         pretty: false,
         compileDebug: true
     };
-    gulp.src('./src/html/**/*.pug')
-        .pipe(filter(file => !/\/_/.test(file.path) && !/^_/.test(file.relative)))
-        .pipe(plumber())
-        .pipe(pug(pugOpts))
-        .pipe(gulp.dest('./dist/'))
+    gulp.src('./src/html/**/*.pug').pipe(filter(file => !/\/_/.test(file.path) && !/^_/.test(file.relative))).pipe(plumber()).pipe(pug(pugOpts)).pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('webCss', () => {
-    const sassOpts = { outputStyle: 'nested'};
+    const sassOpts = {
+        outputStyle: 'nested'
+    };
 
-    gulp.src(`./src/css/main.scss`)
-        .pipe(sass(sassOpts).on('error', sass.logError))
-        .pipe(autoprefixer({browsers: ['ff >= 4', 'Chrome >= 19', 'ie >= 9'], cascade: false}))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('./dist/css/'))
+    gulp.src(`./src/css/main.scss`).pipe(sass(sassOpts).on('error', sass.logError)).pipe(autoprefixer({
+        browsers: [
+            'ff >= 4', 'Chrome >= 19', 'ie >= 9'
+        ],
+        cascade: false
+    })).pipe(cleanCSS()).pipe(gulp.dest('./dist/css/'))
 });
 
 gulp.task('webJs', () => {
 
-    gulp.src('./src/js/**/*.es6')
-        .pipe(plumber())
-        .pipe(babel(babelOpts))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js/'))
+    gulp.src('./src/js/**/*.es6').pipe(plumber()).pipe(babel(babelOpts)).pipe(uglify()).pipe(gulp.dest('./dist/js/'))
 });
 
-gulp.task('build', ['webJs','webCss']);
+gulp.task('build', ['webJs', 'webCss', 'webHtml']);
 
 gulp.task('default', () => {
     gulp.watch('./src/css/**/*.scss', ['webCss']);
